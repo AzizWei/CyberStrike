b251a00ed3965b743896d5c270eb573dde141467
wei <tk841213@gmail.com>
2023-06-26 16:18:44 +0800

    contact part

--- js/script.js
+++ js/script.js
@@ -51,3 +51,30 @@ $(document).ready(function() {
 
   
 
+
+
+// contact part
+
+// 控制進度條  只需調整html裡的span數值 下方bar會跟著變化
+var progressSections = document.querySelectorAll(".progress");  //選取所有具有.progress類別的元素
+
+progressSections.forEach((section)=>{    //forEach遍歷每個進度段落
+    var progressValue = section.querySelector("h3 span").innerHTML;  //抓到 h3底下span裡的值
+    var progress = parseInt(progressValue.replace("%", ""));   //parseInt將字串轉換為以十進位表示的整數 並且把值"%"替代為空白
+    var progressBar = section.querySelector(".bar span");   
+    progressBar.style.width = progress + "%";
+});
+//Slide Control
+
+document.getElementById('nextBtn').onclick=()=>{
+    let lists = document.querySelectorAll('.card');
+    document.getElementById('slide').appendChild(lists[0].cloneNode(true));
+    document.getElementById('slide').removeChild(lists[0]);
+}
+
+  
+document.getElementById('preBtn').onclick =()=>{
+    let lists = document.querySelectorAll('.card');
+    document.getElementById('slide').prepend(lists[lists.length - 1]);
+}
+
